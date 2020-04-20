592e32bd67ce2425e49cfb0132781b9d54c88270
eKliman <kliman.evgeniy@gmail.com>
2020-04-20 16:19:34 +0300

    Built a web site by course From 0 to 1

---
+++ Training_site_from_0_to_1/js/main.js
@@ -0,0 +1,14 @@
+// use a function to make sure that HTML is loaded and processed first
+$(function () {
+  $(".slider__inner, .news__slider-inner").slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    infinite: false,
+  });
+
+  $("select").styler();
+
+  $(".header__btn-menu").on("click", function () {
+    $(".menu ul").slideToggle();
+  });
+});
